e846a170752352d90c8cfe08148ad850b26ad99d
offtkp <parisoplop@gmail.com>
2024-10-12 02:19:09 +0300

    Shippable :wrage:

--- src/felix86/backend/allocation_map.hpp
+++ src/felix86/backend/allocation_map.hpp
@@ -12,22 +12,18 @@ struct AllocationMap {
     AllocationMap& operator=(AllocationMap&&) = default;
 
     void Allocate(u32 name, biscuit::GPR gpr) {
-        assertName(name);
         allocations[name] = gpr;
     }
 
     void Allocate(u32 name, biscuit::FPR fpr) {
-        assertName(name);
         allocations[name] = fpr;
     }
 
     void Allocate(u32 name, biscuit::Vec vec) {
-        assertName(name);
         allocations[name] = vec;
     }
 
     void Allocate(u32 name, u32 spill, SpillSize size, AllocationType type) {
-        assertName(name);
         allocations[name] = Allocation{spill, size, type};
     }
 
@@ -38,9 +34,5 @@ struct AllocationMap {
     }
 
 private:
-    void assertName(u32 name) {
-        ASSERT(allocations.find(name) == allocations.end());
-    }
-
     tsl::robin_map<u32, Allocation> allocations;
 };
