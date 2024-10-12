f027c0cc9a743f53a790cf4b52d1d23c46c37eb0
offtkp <parisoplop@gmail.com>
2024-10-12 17:46:52 +0300

    Whoopsies

--- src/felix86/emulator.cpp
+++ src/felix86/emulator.cpp
@@ -180,6 +180,7 @@ void* Emulator::compileFunction(u64 rip) {
     bool changed = false;
 
     do {
+        changed = false;
         changed |= PassManager::PeepholePass(&function);
         changed |= PassManager::LocalCSEPass(&function);
         changed |= PassManager::CopyPropagationPass(&function);
