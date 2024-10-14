cbeaa134ccaf147f4f93605ab740ea3ed4a32569
offtkp <parisoplop@gmail.com>
2024-10-14 21:10:22 +0300

    Split up

--- tests/FEX/primary.cpp
+++ tests/FEX/primary.cpp
@@ -1,17 +1,42 @@
 #include <catch2/catch_test_macros.hpp>
 #include "FEX/fex_test_loader.hpp"
 
-TEST_CASE("Primary", "[FEX]") {
+TEST_CASE("Primary_00", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_00.asm");
+}
+
+TEST_CASE("Primary_08", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_08.asm");
-    // FEXTestLoader::RunTest("ASM/Primary/Primary_10.asm"); ADC/SBB, just a nuisance that is never used, TODO
-    // FEXTestLoader::RunTest("ASM/Primary/Primary_18.asm");
+}
+
+TEST_CASE("Primary_20", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_20.asm");
+}
+
+TEST_CASE("Primary_28", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_28.asm");
+}
+
+TEST_CASE("Primary_30", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_30.asm");
+}
+
+TEST_CASE("Primary_38", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_38.asm");
+}
+
+TEST_CASE("Primary_50", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_50.asm");
+}
+
+TEST_CASE("Primary_50_2", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_50_2.asm");
+}
+
+TEST_CASE("Primary_8D", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_8D.asm");
+}
+
+TEST_CASE("Primary_8D_2", "[FEX][Primary]") {
     FEXTestLoader::RunTest("ASM/Primary/Primary_8D_2.asm");
 }
