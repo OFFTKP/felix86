cffdd1ee2973f736071f2d13499ab8123fadc8df
offtkp <parisoplop@gmail.com>
2024-10-16 13:58:21 +0300

    Better test failure maybe

--- tests/FEX/fex_test_loader.cpp
+++ tests/FEX/fex_test_loader.cpp
@@ -10,6 +10,8 @@
 #include "nlohmann/json.hpp"
 
 FEXTestLoader::FEXTestLoader(const std::filesystem::path& path) {
+    g_testing = true;
+
     std::filesystem::path cpath = std::filesystem::absolute(path);
     if (!std::filesystem::exists(cpath)) {
         ERROR("File does not exist: %s", cpath.string().c_str());
