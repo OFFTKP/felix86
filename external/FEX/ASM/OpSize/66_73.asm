86bf354076a55ac6bc1597720eb5e3dc0d85fe7f
offtkp <parisoplop@gmail.com>
2024-11-04 22:05:43 +0200

    add test

---
+++ external/FEX/ASM/OpSize/66_73.asm
@@ -0,0 +1,35 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "XMM0": ["0x0000414243444546", "0x0000717273747576"],
+    "XMM1": ["0x4142434445460000", "0x7172737475760000"],
+    "XMM2": ["0x0000414243444546", "0x0000717273747576"],
+    "XMM3": ["0x4142434445460000", "0x7172737475760000"],
+  },
+  "MemoryRegions": {
+    "0x100000000": "4096"
+  }
+}
+%endif
+bits 64
+
+mov rdx, 0xe0000000
+
+mov rax, 0x7172737475767778
+mov [rdx + 8 * 0], rax
+mov rax, 0x4142434445464748
+mov [rdx + 8 * 1], rax
+
+movapd xmm0, [rdx]
+psrlq xmm0, 16
+
+movapd xmm1, [rdx]
+psllq xmm1, 16
+
+movapd xmm2, [rdx]
+psrldq xmm2, 2
+
+movapd xmm3, [rdx]
+pslldq xmm3, 2
+
+hlt
