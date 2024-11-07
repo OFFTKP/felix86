a2ba7240334cb67395df0428c7a151f390bb7cb2
offtkp <parisoplop@gmail.com>
2024-11-08 00:06:17 +0200

    test

--- src/felix86/common/global.hpp
+++ src/felix86/common/global.hpp
@@ -25,6 +25,7 @@ extern u64 g_executable_start, g_executable_end;
 extern thread_local ThreadState* g_thread_state;
 extern u64 g_interpreter_base_hint;
 extern u64 g_executable_base_hint;
+extern const char* g_git_hash;
 
 bool parse_extensions(const char* ext);
 void initialize_globals();
