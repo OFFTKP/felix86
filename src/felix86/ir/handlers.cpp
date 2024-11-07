38e0bace033a03e4e85d19a7d6f3898926419d9f
offtkp <parisoplop@gmail.com>
2024-11-07 21:03:27 +0200

    Fix bsr?

--- src/felix86/ir/handlers.cpp
+++ src/felix86/ir/handlers.cpp
@@ -1088,7 +1088,7 @@ IR_HANDLE(bsr) { // bsr - 0x0f 0xbd
     SSAInstruction* clz = ir.Clz(rm);
     // CLZ always deals on 64-bit values, so we need to subtract the result from 63
     SSAInstruction* sub = ir.Sub(ir.Imm(63), clz);
-    ir.SetReg(inst->operand_reg, sub);
+    ir.SetReg(inst->operand_reg, ir.Select(zero, ir.Imm(-1ull), sub));
     ir.SetFlag(zero, X86_REF_ZF);
 }
 
