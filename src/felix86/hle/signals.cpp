bd22572b3ce7c465a2b607790da0bb015dd63e9e
offtkp <parisoplop@gmail.com>
2024-11-21 19:43:27 +0200

    Don't install handlers

--- src/felix86/hle/signals.cpp
+++ src/felix86/hle/signals.cpp
@@ -6,28 +6,29 @@ void signal_handler(int sig, siginfo_t* info, void* ctx) {
 }
 
 SignalHandler::SignalHandler() : handlers(32) {
-    struct sigaction act;
-    act.sa_sigaction = signal_handler;
-    act.sa_flags = SA_SIGINFO;
-    sigemptyset(&act.sa_mask);
-    for (int i = 1; i < 32; i++) {
-        sigaction(i, &act, nullptr);
-    }
+    // struct sigaction act;
+    // act.sa_sigaction = signal_handler;
+    // act.sa_flags = SA_SIGINFO;
+    // sigemptyset(&act.sa_mask);
+    // for (int i = 1; i < 32; i++) {
+    //     sigaction(i, &act, nullptr);
+    // }
 }
 
 SignalHandler::~SignalHandler() {
-    struct sigaction act;
-    act.sa_handler = SIG_DFL;
-    act.sa_flags = 0;
-    sigemptyset(&act.sa_mask);
-    for (int i = 1; i < 32; i++) {
-        sigaction(i, &act, nullptr);
-    }
+    // struct sigaction act;
+    // act.sa_handler = SIG_DFL;
+    // act.sa_flags = 0;
+    // sigemptyset(&act.sa_mask);
+    // for (int i = 1; i < 32; i++) {
+    //     sigaction(i, &act, nullptr);
+    // }
 }
 
 RegisteredSignal SignalHandler::RegisterSignalHandler(int sig, void* handler, sigset_t mask, int flags) {
     ASSERT(sig > 0 && sig < 32);
     RegisteredSignal old = handlers[sig];
     handlers[sig] = {handler, mask, flags};
+    WARN("Registering signal handler for signal %d", sig);
     return old;
 }
