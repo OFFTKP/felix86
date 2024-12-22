712beb52dfec56c58365e93dd9be8b1150f54ba1
offtkp <parisoplop@gmail.com>
2024-12-22 03:59:22 +0200

    comiss

--- src/felix86/ir/handlers.cpp
+++ src/felix86/ir/handlers.cpp
@@ -944,6 +944,69 @@ IR_HANDLE(mov_xmm128_xmm) { // movups/movaps xmm128, xmm - 0x0f 0x29
     ir.SetRm(inst->operand_rm, reg, VectorState::PackedByte);
 }
 
+IR_HANDLE(comiss) {
+    ir.SetFlag(ir.Imm(0), X86_REF_OF);
+    ir.SetFlag(ir.Imm(0), X86_REF_SF);
+    ir.SetFlag(ir.Imm(0), X86_REF_AF);
+
+    IRBlock* next_instruction_target = ir.CreateBlockAt(ir.GetNextAddress());
+    SSAInstruction* xmm1 = ir.GetReg(inst->operand_reg);
+    SSAInstruction* xmm2 = ir.GetRm(inst->operand_rm, VectorState::Float);
+    // NaN is not equal itself
+    SSAInstruction* is_nan1 = ir.VToI(ir.VFNotEqual(xmm1, xmm1, VectorState::Float), VectorState::Float);
+    SSAInstruction* is_nan2 = ir.VToI(ir.VFNotEqual(xmm2, xmm2, VectorState::Float), VectorState::Float);
+    SSAInstruction* is_nan = ir.Or(is_nan1, is_nan2);
+
+    IRBlock* nan_block = ir.CreateBlock();
+    IRBlock* not_nan_block = ir.CreateBlock();
+
+    ir.TerminateJumpConditional(is_nan, nan_block, not_nan_block);
+    ir.SetBlock(nan_block);
+
+    ir.SetFlag(ir.Imm(1), X86_REF_ZF);
+    ir.SetFlag(ir.Imm(1), X86_REF_PF);
+    ir.SetFlag(ir.Imm(1), X86_REF_CF);
+
+    ir.TerminateJump(next_instruction_target);
+    ir.SetBlock(not_nan_block);
+
+    SSAInstruction* not_equal = ir.VToI(ir.VFNotEqual(xmm1, xmm2, VectorState::Float), VectorState::Float);
+
+    IRBlock* equal_block = ir.CreateBlock();
+    IRBlock* not_equal_block = ir.CreateBlock();
+
+    ir.TerminateJumpConditional(not_equal, not_equal_block, equal_block);
+    ir.SetBlock(equal_block);
+
+    ir.SetFlag(ir.Imm(1), X86_REF_ZF);
+    ir.SetFlag(ir.Imm(0), X86_REF_PF);
+    ir.SetFlag(ir.Imm(0), X86_REF_CF);
+
+    ir.TerminateJump(next_instruction_target);
+    ir.SetBlock(not_equal_block);
+
+    SSAInstruction* less = ir.VToI(ir.VFLessThan(xmm1, xmm2, VectorState::Float), VectorState::Float);
+
+    IRBlock* less_block = ir.CreateBlock();
+    IRBlock* greater_block = ir.CreateBlock();
+
+    ir.TerminateJumpConditional(less, less_block, greater_block);
+    ir.SetBlock(less_block);
+
+    ir.SetFlag(ir.Imm(0), X86_REF_ZF);
+    ir.SetFlag(ir.Imm(0), X86_REF_PF);
+    ir.SetFlag(ir.Imm(1), X86_REF_CF);
+
+    ir.TerminateJump(next_instruction_target);
+    ir.SetBlock(greater_block);
+
+    ir.SetFlag(ir.Imm(0), X86_REF_ZF);
+    ir.SetFlag(ir.Imm(0), X86_REF_PF);
+    ir.SetFlag(ir.Imm(0), X86_REF_CF);
+
+    ir.TerminateJump(next_instruction_target);
+}
+
 IR_HANDLE(mov_xmm_m64) { // movlpd xmm, m64 - 0x66 0x0f 0x12
     // Just load a double from memory directly into an xmm - thus using vector loads
     // instead of gpr loads and then moving to vector
