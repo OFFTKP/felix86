a457dc844cd5435fd339b35f5efc4312d182ab7b
offtkp <parisoplop@gmail.com>
2025-01-21 03:18:10 +0200

    test

--- tests/ASM/felix86_bugs/test_high.asm
+++ tests/ASM/felix86_bugs/test_high.asm
@@ -1,7 +1,8 @@
 %ifdef CONFIG
 {
   "RegData": {
-    "RBX": "0x0000000000000001"
+    "RBX": "0x0000000000000001",
+    "RCX": "0x1234"
   }
 }
 %endif
@@ -11,5 +12,9 @@ xor ebx, ebx
 mov rax, 0x8000
 test ah, 0x80
 setne bl
+
+mov rax, 0x12
+mov rcx, 0x34
+or ch, al
 
 hlt
