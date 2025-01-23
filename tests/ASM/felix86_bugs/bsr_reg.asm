c78ebcda8ba67bc13cee9369dbb27e65447c0e96
offtkp <parisoplop@gmail.com>
2025-01-23 02:36:51 +0200

    x

--- tests/ASM/felix86_bugs/bsr_reg.asm
+++ tests/ASM/felix86_bugs/bsr_reg.asm
@@ -15,10 +15,30 @@
 %endif
 bits 64
 
+mov rax, 0xFFFFFFFFFFFFFFFF
+mov ebx, 0x1010101010101010
 mov ecx, 0
 
+mov r15, -1
+mov r14, -1
+mov r13, -1
+mov r12, -1
+mov r11, -1
+mov r10, -1
+mov r9,  -1
+mov r8,  -1
 mov rsi, -1
 
+bsr r15w, ax
+bsr r14d, eax
+bsr r13,  rax
+
+bsr r12w, bx
+bsr r11d, ebx
+bsr r10,  rbx
+
+bsr r9w, cx
+bsr r8d, ecx
 bsr rsi, rcx
 
 hlt
