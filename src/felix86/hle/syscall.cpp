018722fc3eed18ac1043e26e7e109f4d7ec7c98f
offtkp <parisoplop@gmail.com>
2025-01-30 21:11:07 +0200

    test

--- src/felix86/hle/syscall.cpp
+++ src/felix86/hle/syscall.cpp
@@ -282,16 +282,6 @@ void felix86_syscall(ThreadState* state) {
         STRACE("recvmmsg(%d, %p, %d, %d, %d) = %d", (int)rdi, (void*)rsi, (int)rdx, (int)r10, (int)r8, (int)result);
         break;
     }
-    case felix86_x86_64_sendmsg: {
-        result = HOST_SYSCALL(sendmsg, rdi, (struct msghdr*)rsi, rdx);
-        STRACE("sendmsg(%d, %p, %d) = %d", (int)rdi, (void*)rsi, (int)rdx, (int)result);
-        break;
-    }
-    case felix86_x86_64_recvmsg: {
-        result = HOST_SYSCALL(recvmsg, rdi, (struct msghdr*)rsi, rdx);
-        STRACE("recvmsg(%d, %p, %d) = %d", (int)rdi, (void*)rsi, (int)rdx, (int)result);
-        break;
-    }
     case felix86_x86_64_setsockopt: {
         result = HOST_SYSCALL(setsockopt, rdi, rsi, rdx, r10, r8);
         STRACE("setsockopt(%d, %d, %d, %d, %d) = %d", (int)rdi, (int)rsi, (int)rdx, (int)r10, (int)r8, (int)result);
