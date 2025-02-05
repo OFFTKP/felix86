cc303b18a69c2f8b3ae2b9e707e1f07c1c14ee3b
offtkp <parisoplop@gmail.com>
2025-02-05 17:56:40 +0200

    test

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -3191,10 +3191,10 @@ FAST_HANDLE(PSHUFHW) {
     rec.setVectorState(SEW::E16, rec.maxVlen() / 16);
     AS.VMV(dst, src); // to move the low words
 
-    u8 el0 = imm & 0b11;
-    u8 el1 = (imm >> 2) & 0b11;
-    u8 el2 = (imm >> 4) & 0b11;
-    u8 el3 = (imm >> 6) & 0b11;
+    u8 el0 = 4 + (imm & 0b11);
+    u8 el1 = 4 + (imm >> 2) & 0b11;
+    u8 el2 = 4 + (imm >> 4) & 0b11;
+    u8 el3 = 4 + (imm >> 6) & 0b11;
     AS.VMV(iota2, el3);
     AS.LI(tmp, el2);
     AS.VSLIDE1UP(iota, iota2, tmp);
