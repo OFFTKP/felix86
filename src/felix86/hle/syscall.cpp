eeed5573d76e94e61af03a3b3940697b4e991d0f
offtkp <parisoplop@gmail.com>
2025-02-06 23:48:51 +0200

    utime

--- src/felix86/hle/syscall.cpp
+++ src/felix86/hle/syscall.cpp
@@ -633,16 +633,8 @@ void felix86_syscall(ThreadState* state) {
         break;
     }
     case felix86_x86_64_utimensat: {
-        auto path = fs.AtPath(rdi, (const char*)rsi);
-
-        if (!path) {
-            STRACE("utimensat(%d, %s, %p, %d) = %d", (int)rdi, (char*)rsi, (void*)rdx, (int)r10, -EACCES);
-            result = -EACCES;
-            break;
-        }
-
-        result = HOST_SYSCALL(utimensat, rdi, path->c_str(), (struct timespec*)rdx, r10);
-        STRACE("utimensat(%d, %s, %p, %d) = %d", (int)rdi, path->c_str(), (void*)rdx, (int)r10, (int)result);
+        result = HOST_SYSCALL(utimensat, rdi, (const char*)rsi, (struct timespec*)rdx, r10);
+        STRACE("utimensat(%d, %s, %p, %d) = %d", (int)rdi, (const char*)rsi, (void*)rdx, (int)r10, (int)result);
         break;
     }
     case felix86_x86_64_getgid: {
