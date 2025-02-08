a89ecf14268145f3fea5d0c490da805d223dd642
offtkp <parisoplop@gmail.com>
2025-02-08 23:40:34 +0200

    stosb

--- src/felix86/v2/recompiler.cpp
+++ src/felix86/v2/recompiler.cpp
@@ -252,31 +252,31 @@ u64 Recompiler::compileSequence(u64 rip) {
         }
 
         // When we want to print all instructions used
-        static std::unordered_map<std::string, bool> seen;
+        // static std::unordered_map<std::string, bool> seen;
 
-        static bool start = false;
+        // static bool start = false;
 
-        if (rip == 0x103863) {
-            start = true;
-        }
+        // if (rip == 0x103863) {
+        //     start = true;
+        // }
 
-        if (rip == 0x10386C) {
-            fflush(stdout);
-            raise(SIGTRAP);
-            exit(1);
-        }
+        // if (rip == 0x10386C) {
+        //     fflush(stdout);
+        //     raise(SIGTRAP);
+        //     exit(1);
+        // }
 
-        if (start) {
-            ZydisDisassembledInstruction disassembled;
-            ZydisDisassembleIntel(ZYDIS_MACHINE_MODE_LONG_64, meta.rip, (u8*)meta.rip, 15, &disassembled);
-            std::string instr = disassembled.text;
-            if (seen.find(instr) == seen.end()) {
-                seen[instr] = true;
-                fflush(stdout);
-                PLAIN("%s", instr.c_str());
-                fflush(stdout);
-            }
-        }
+        // if (start) {
+        //     ZydisDisassembledInstruction disassembled;
+        //     ZydisDisassembleIntel(ZYDIS_MACHINE_MODE_LONG_64, meta.rip, (u8*)meta.rip, 15, &disassembled);
+        //     std::string instr = disassembled.text;
+        //     if (seen.find(instr) == seen.end()) {
+        //         seen[instr] = true;
+        //         fflush(stdout);
+        //         PLAIN("%s", instr.c_str());
+        //         fflush(stdout);
+        //     }
+        // }
 
         // Checks that we didn't forget to emulate any flags
         // if (g_paranoid && mnemonic != ZYDIS_MNEMONIC_SYSCALL) {
