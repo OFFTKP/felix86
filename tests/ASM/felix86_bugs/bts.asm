dcc6bef371c22125f66bae6a1bef10abe02ef0b9
offtkp <parisoplop@gmail.com>
2025-02-09 15:12:22 +0200

    bts

--- tests/ASM/felix86_bugs/bts.asm
+++ tests/ASM/felix86_bugs/bts.asm
@@ -2,7 +2,9 @@
 {
   "RegData": {
     "R14":"0x2000",
-    "R13":"0x4000"
+    "R13":"0x10",
+    "R12":"0x4000",
+    "R11":"0xC000"
   }
 }
 %endif
@@ -24,8 +26,9 @@ or r15, rax
 xor r15, r15
 
 mov rdi, 0xe0000000
-mov rax, 0
+mov rax, 0x10
 mov [rdi], rax
+mov rax, 0
 mov [rdi+8], rax
 mov [rdi+16], rax
 mov [rdi+32], rax
@@ -39,8 +42,20 @@ mov r14, rcx
 cfmerge
 
 clc
-bts qword[rdi], 192 + 14
+bts qword[rdi], 64 * 3 + 4 ; this gets masked and it's actually rdi bit 4
 mov r13, [rdi]
+cfmerge
+
+clc
+mov rax, 192 + 14
+bts qword[rdi], rax ; this indexes the 206th bit
+mov r12, [rdi + 24]
+cfmerge
+
+clc
+mov rax, 192 + 15
+bts qword[rdi], rax ; this indexes the 207th bit, not set
+mov r11, [rdi + 24]
 cfmerge
 
 
