9f0a5a10f90e549016f4802c2f25de206b384097
offtkp <parisoplop@gmail.com>
2025-02-09 16:44:54 +0200

    boop?

---
+++ tests/ASM/felix86_bugs/packsswb.asm
@@ -0,0 +1,21 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "XMM0": ["0x7f8080807f808080", "0x7f8080807f808080"]
+  }
+}
+%endif
+
+bits 64
+
+; 0x7fff800080008000, 0x7fff800080008000
+mov rax, 0x7fff800080008000
+mov rdi, 0xe0000000
+mov [rdi], rax
+mov [rdi + 8], rax
+
+movaps xmm3, [rdi]
+
+packsswb xmm0, xmm3
+
+hlt
