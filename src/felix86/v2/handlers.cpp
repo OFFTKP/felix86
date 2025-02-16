836ae3da81a53238885296230712f28d23f5f976
offtkp <parisoplop@gmail.com>
2025-02-17 01:17:11 +0200

    x

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -4435,6 +4435,7 @@ void COMIS(Recompiler& rec, const HandlerMetadata& meta, ZydisDecodedInstruction
     AS.SB(cf, offsetof(ThreadState, pf), rec.threadStatePointer());
 
     AS.Bind(&end);
+    rec.writebackDirtyState();
 }
 
 FAST_HANDLE(COMISD) { // Fuzzed
