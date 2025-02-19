11181f16683ce4e53c46acc3b96a32f16b2971d9
offtkp <parisoplop@gmail.com>
2025-02-19 22:03:08 +0200

    op

--- src/felix86/common/elf.cpp
+++ src/felix86/common/elf.cpp
@@ -32,8 +32,6 @@ Elf::~Elf() {
 }
 
 void Elf::Load(const std::filesystem::path& path) {
-    return LoadOld(path);
-
     if (!std::filesystem::exists(path)) {
         WARN("File %s does not exist", path.c_str());
         return;
@@ -244,6 +242,7 @@ void Elf::Load(const std::filesystem::path& path) {
     }
 
     if (!is_interpreter) {
+        // TODO: ???????????????????????????????????? VVVVVVV ?????????????? wth is this highest_vaddr fix me
         g_current_brk = (u64)mmap((void*)PAGE_ALIGN(highest_vaddr), brk_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
         if ((void*)g_current_brk == MAP_FAILED) {
             ERROR("Failed to allocate memory for brk in file %s", path.c_str());
