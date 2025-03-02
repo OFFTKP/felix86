13e8873fd7ee0f964d28dfa86b063e935c24e312
offtkp <parisoplop@gmail.com>
2025-03-02 03:17:27 +0200

    why

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -487,7 +487,10 @@ FAST_HANDLE(CALL_rsb) {
     //     rec.backToDispatcher(true); // true = push to rsb
     // }
     rec.backToDispatcher();
-    // rec.stopCompiling();
+    as.GetCodeBuffer().Emit32(0);
+    as.GetCodeBuffer().Emit32(0);
+    as.GetCodeBuffer().Emit32(0);
+    as.GetCodeBuffer().Emit32(0);
     u64 here = (u64)as.GetCursorPointer();
     ASSERT(here == start + 20);
 }
