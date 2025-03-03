2f6c3cd53cd50ee48dd66bf9fe1797627fb4c2cf
offtkp <parisoplop@gmail.com>
2025-03-03 15:58:31 +0200

    test this

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -2160,7 +2160,7 @@ FAST_HANDLE(MOVDQU) {
 FAST_HANDLE(RDTSC) {
     biscuit::GPR tsc = rec.scratch();
     as.RDTIME(tsc);
-    as.SRLI(tsc, tsc, 2);
+    as.SLLI(tsc, tsc, 7);
     rec.setRefGPR(X86_REF_RAX, X86_SIZE_DWORD, tsc);
     as.SRLI(tsc, tsc, 32);
     rec.setRefGPR(X86_REF_RDX, X86_SIZE_DWORD, tsc);
