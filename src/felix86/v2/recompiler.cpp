15ec6397c2df475910061e28064e37896a4248b1
offtkp <parisoplop@gmail.com>
2025-03-25 02:59:12 +0200

    boop

--- src/felix86/v2/recompiler.cpp
+++ src/felix86/v2/recompiler.cpp
@@ -285,7 +285,7 @@ HostAddress Recompiler::compile(ThreadState* state, HostAddress rip) {
 
         BlockMetadata& metadata = getBlockMetadata(rip);
         std::string symbol = get_perf_symbol(rip.raw());
-        static char buffer[4096];
+        char buffer[4096];
         size_t size = metadata.address_end.raw() - metadata.address.raw();
         int string_size = snprintf(buffer, 4096, "%lx %lx %s\n", metadata.address.raw(), size, symbol.c_str());
         ASSERT(string_size > 0 && string_size < 4095);
