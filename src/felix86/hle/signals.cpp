dd5da52a3381d03a2b09a8e3886f9766de6bb4b5
offtkp <parisoplop@gmail.com>
2025-04-01 01:10:48 +0300

    ???

--- src/felix86/hle/signals.cpp
+++ src/felix86/hle/signals.cpp
@@ -974,7 +974,7 @@ void Signals::registerSignalHandler(ThreadState* state, int sig, GuestAddress ha
         sigemptyset(&sa.sa_mask);
 
         // The libc `sigaction` function fails when you try to modify handlers for SIG33 for example
-        if (syscall(SYS_rt_sigaction, sig, &sa, nullptr, sizeof(sigset_t) / 8) != 0) {
+        if (syscall(SYS_rt_sigaction, sig, &sa, nullptr, 8) != 0) {
             WARN("Failed when setting signal handler for signal: %d (%s)", sig, strsignal(sig));
         }
     }
