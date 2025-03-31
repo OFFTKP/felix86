9921cecbabc28c47b26d9cb323b07fe0be8eba56
offtkp <parisoplop@gmail.com>
2025-04-01 02:02:29 +0300

    test

--- src/felix86/hle/syscall.cpp
+++ src/felix86/hle/syscall.cpp
@@ -1151,11 +1151,10 @@ Result felix86_syscall_common(ThreadState* state, int rv_syscall, u64 arg1, u64 
                 break;
             }
 
-            sigset_t host_mask;
-            printf("Setting mask to %lx\n", state->signal_mask);
-            sigandset(&host_mask, &state->signal_mask, Signals::hostSignalMask());
-            int result = pthread_sigmask(SIG_SETMASK, &host_mask, nullptr);
-            ASSERT(result == 0);
+            // sigset_t host_mask;
+            // sigandset(&host_mask, &state->signal_mask, Signals::hostSignalMask());
+            // int result = pthread_sigmask(SIG_SETMASK, &host_mask, nullptr);
+            // ASSERT(result == 0);
         }
 
         if (oldset) {
