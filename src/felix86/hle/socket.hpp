4d55b2786c63373f8d4982177a8c29ff75433964
offtkp <parisoplop@gmail.com>
2025-04-10 14:48:32 +0300

    Boop

--- src/felix86/hle/socket.hpp
+++ src/felix86/hle/socket.hpp
@@ -2,6 +2,6 @@
 
 #include "felix86/hle/guest_types.hpp"
 
-int recvmsg32(int fd, const x86_msghdr* msg, int flags);
+int recvmsg32(int fd, x86_msghdr* msg, int flags);
 
 int sendmsg32(int fd, const x86_msghdr* msg, int flags);
