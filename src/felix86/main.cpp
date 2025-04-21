acaf5254ca4ab49e2b8ce43d672ecb5110b23670
offtkp <parisoplop@gmail.com>
2025-04-22 00:40:36 +0300

    whoops

--- src/felix86/main.cpp
+++ src/felix86/main.cpp
@@ -225,7 +225,7 @@ void kill_all() {
     }
 
     if (!at_least_one) {
-        printf("No instances of the emulator are running, at least from the path %s\n", exe_path);
+        printf("No instances of the emulator are found\n");
     }
 
     fclose(pgrep_output);
