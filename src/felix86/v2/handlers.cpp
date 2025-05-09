59bcee753f9b32ae86859da8681bdc0c52be1a77
offtkp <parisoplop@gmail.com>
2025-05-09 03:00:07 +0300

    optimize div

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -1554,6 +1554,7 @@ FAST_HANDLE(DIV) {
         as.SLLI(edx, edx, 32);
         as.OR(edx, edx, eax);
 
+        // This order is okay as RDX is modified last
         as.DIVU(eax, edx, src);
         as.REMU(edx, edx, src);
 
@@ -1562,11 +1563,38 @@ FAST_HANDLE(DIV) {
         break;
     }
     case X86_SIZE_QWORD: {
+        // Most of the time the program doesn't actually want a 128-bit divide
+        // and instead wants a 64-bit divide. If RDX is sign-extended from RAX then
+        // we can use our RISC-V divide instruction rather than calling a function to emulate 128-bit div
+        biscuit::GPR rax = rec.getRefGPR(X86_REF_RAX, X86_SIZE_QWORD);
+        biscuit::GPR rdx = rec.getRefGPR(X86_REF_RDX, X86_SIZE_QWORD);
+        biscuit::GPR mod = rec.scratch();
+        biscuit::GPR div = rec.scratch();
+        biscuit::Label do_64bit, end;
+
+        // Trick to check if val == 0 || val == -1
+        biscuit::GPR scratch = rec.scratch();
+        as.ADDI(scratch, rdx, 1);
+        as.SLTIU(scratch, scratch, 2);
+        as.BNEZ(scratch, &do_64bit);
+
+        // We need a slow 128-bit divide...
         rec.writebackState();
         as.MV(a1, src);
         as.MV(a0, rec.threadStatePointer());
         rec.call((u64)&felix86_divu128);
         rec.restoreState();
+        as.J(&end);
+
+        as.Bind(&do_64bit);
+
+        as.DIVU(div, rax, src);
+        as.REMU(mod, rax, src);
+
+        rec.setRefGPR(X86_REF_RAX, X86_SIZE_QWORD, div);
+        rec.setRefGPR(X86_REF_RDX, X86_SIZE_QWORD, mod);
+
+        as.Bind(&end);
         break;
     }
     default: {
