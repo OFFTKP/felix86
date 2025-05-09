f65179bff8b753dd70179c7695df6d31cc47c164
offtkp <parisoplop@gmail.com>
2025-05-09 22:36:51 +0300

    boop

--- src/felix86/v2/recompiler.cpp
+++ src/felix86/v2/recompiler.cpp
@@ -141,17 +141,16 @@ void Recompiler::emitDispatcher() {
         biscuit::GPR temp2 = scratch();
         biscuit::GPR rip = scratch();
         biscuit::GPR mask = scratch();
+        biscuit::GPR ripreg = allocatedGPR(X86_REF_RIP);
         biscuit::Label not_equal;
-        as.LD(temp2, offsetof(ThreadState, rip), threadStatePointer());
         as.LI(temp, (u64)address_cache.data());
         as.LI(mask, (1 << address_cache_bits) - 1);
-        as.MV(rip, temp2);
-        as.AND(temp2, temp2, mask);
+        as.AND(temp2, ripreg, mask);
         // Multiply by 16, which is size of each address cache entry
         as.SLLI(temp2, temp2, 4);
         as.ADD(temp, temp, temp2);
         as.LD(temp2, 8, temp); // read the AddressCacheEntry::guest field
-        as.BNE(rip, temp2, &not_equal);
+        as.BNE(rip, ripreg, &not_equal);
 
         // Address cache was correct, jump to host address
         as.LD(rip, 0, temp);
