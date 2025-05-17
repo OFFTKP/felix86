4ffa353fc6d660f853ab01ef7df080e081706b7c
offtkp <parisoplop@gmail.com>
2025-05-18 00:39:39 +0300

    boop

--- src/felix86/main.cpp
+++ src/felix86/main.cpp
@@ -402,7 +402,6 @@ int main(int argc, char* argv[]) {
             std::vector<std::string> envs = split_string(guest_envs, ',');
             for (auto& env : envs) {
                 params.envp.push_back(env);
-                printf("Added %s\n", env.c_str());
             }
         }
     } else {
