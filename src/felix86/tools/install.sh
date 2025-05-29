351945a146b6f700aef81cad8c250f1f713a0e43
offtkp <parisoplop@gmail.com>
2025-05-29 15:06:04 +0300

    Update script

--- src/felix86/tools/install.sh
+++ src/felix86/tools/install.sh
@@ -65,8 +65,24 @@ rm /tmp/felix86_artifact/archive.zip
 echo "Downloaded"
 echo "Moving felix86 artifact to $FILE, requesting permission..."
 sudo mkdir -p $INSTALLATION_DIR
+
+# Save the old installation in /tmp/felix86_artifact/old.<random> in case the user wants it back
+TEMP_OLD=$(mktemp /tmp/felix86_artifact/old.XXXXXX)
+if [ -f "/usr/bin/felix86" ]; then
+    sudo mv /usr/bin/felix86 $TEMP_OLD/felix86.ln.bak
+fi
+if [ -f "$FILE" ]; then
+    sudo mv $FILE $TEMP_OLD/felix86.opt.bak
+fi
+if [ -d "$INSTALLATION_DIR/lib" ]
+    sudo mv $INSTALLATION_DIR/lib $TEMP_OLD/
+fi
+
 sudo mv /tmp/felix86_artifact/felix86 $FILE
-echo "Successfully installed felix86 at $FILE"
+sudo mv /tmp/felix86_artifact/lib $INSTALLATION_DIR
+sudo ln -s $FILE /usr/bin/felix86
+echo "Successfully installed felix86 at $FILE and libraries at $INSTALLATION_DIR/lib"
+felix86 --set-thunks $INSTALLATION_DIR/lib
 
 if [[ "$exit_after_install" == "1" ]]; then
     exit
