274466516b601bb99bdf62d6eeeba11abd57aa89
offtkp <parisoplop@gmail.com>
2025-06-04 17:22:52 +0300

    boop

--- src/felix86/hle/abi.cpp
+++ src/felix86/hle/abi.cpp
@@ -3,8 +3,8 @@
 #include "felix86/common/state.hpp"
 #include "felix86/common/types.hpp"
 #include "felix86/common/utility.hpp"
-#include "felix86/emulator.hpp"
 #include "felix86/hle/abi.hpp"
+#include "felix86/hle/signals.hpp"
 #include "felix86/v2/recompiler.hpp"
 
 biscuit::GPR gprarg(int i) {
