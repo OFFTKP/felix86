07c722826b28d1ca8c96a126d0fefb625d846d42
offtkp <parisoplop@gmail.com>
2025-06-05 23:23:33 +0300

    .

--- src/felix86/common/perf.hpp
+++ src/felix86/common/perf.hpp
@@ -13,9 +13,6 @@ struct Perf {
             ASSERT(fd > 0);
         } else if (g_config.perf_blocks || g_config.perf_libs || g_config.perf_global) {
             WARN("Failed to open perf map file for process %d", getpid());
-            g_config.perf_blocks = false;
-            g_config.perf_libs = false;
-            g_config.perf_global = false;
         } else {
             WARN("File: %s", path.c_str());
         }
@@ -26,9 +23,11 @@ struct Perf {
     }
 
     void addToFile(unsigned long address, unsigned long size, const std::string& symbol) {
-        std::string full = fmt::format("{:x} {:x} {}\n", address, size, symbol);
-        int written = syscall(SYS_write, fd, full.data(), full.size());
-        ASSERT_MSG(written == (int)full.size(), "%lx != %lx (errno: %d)", written, full.size(), errno);
+        if (f) {
+            std::string full = fmt::format("{:x} {:x} {}\n", address, size, symbol);
+            int written = syscall(SYS_write, fd, full.data(), full.size());
+            ASSERT_MSG(written == (int)full.size(), "%lx != %lx (errno: %d)", written, full.size(), errno);
+        }
     }
 
 private:
