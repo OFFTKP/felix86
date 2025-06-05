3b7cbb3b6005ee623e84fdda13bc35bac2ecc185
offtkp <parisoplop@gmail.com>
2025-06-05 23:29:20 +0300

    boop

--- src/felix86/common/perf.hpp
+++ src/felix86/common/perf.hpp
@@ -12,14 +12,16 @@ struct Perf {
             fd = fileno(f);
             ASSERT(fd > 0);
         } else if (g_config.perf_blocks || g_config.perf_libs || g_config.perf_global) {
+            // TODO: for some reason, running a setuid app through binfmt_misc after being forked
+            // loves failing the fopen and idk why, but game processes aren't setuid so we can perf them fine
             WARN("Failed to open perf map file for process %d", getpid());
-        } else {
-            WARN("File: %s", path.c_str());
         }
     }
 
     ~Perf() {
-        fclose(f);
+        if (f) {
+            fclose(f);
+        }
     }
 
     void addToFile(unsigned long address, unsigned long size, const std::string& symbol) {
