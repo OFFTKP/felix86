03934e00d9230d63ccf0051143982c4389165d54
offtkp <parisoplop@gmail.com>
2025-06-07 14:53:05 +0300

    .

--- src/felix86/hle/ipc32.cpp
+++ src/felix86/hle/ipc32.cpp
@@ -33,7 +33,7 @@ int ipc32(u32 call, u32 first, u64 second, u64 third, void* ptr, u64 fifth) {
         return ::syscall(SYS_semop, first, (sembuf*)ptr, second);
     }
     case felix86_SEMGET: {
-        PLAIN("semget %lx %lx %lx", first, ptr, second);
+        PLAIN("semget %lx %lx %lx", first, second, third);
         return ::syscall(SYS_semget, first, second, third);
     }
     case felix86_SEMCTL: {
@@ -43,7 +43,7 @@ int ipc32(u32 call, u32 first, u64 second, u64 third, void* ptr, u64 fifth) {
         bool ipc64 = third & 0x100;
         x86_semid_ds_64* ptr64 = (x86_semid_ds_64*)ptr;
         x86_semid_ds_32* ptr32 = (x86_semid_ds_32*)ptr;
-        PLAIN("semget %lx %lx %lx %lx", first, second, third, ptr);
+        PLAIN("semctl %lx %lx %lx %lx", first, second, third, ptr);
         switch (semcmd) {
         case IPC_SET: {
             riscv64_semid64_ds host_semid{};
