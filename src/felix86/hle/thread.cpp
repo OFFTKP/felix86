b29d0a46fdf7cb03ad3a2933fad42c085ad70e27
offtkp <parisoplop@gmail.com>
2025-06-08 20:38:48 +0300

    .

--- src/felix86/hle/thread.cpp
+++ src/felix86/hle/thread.cpp
@@ -318,8 +318,8 @@ long Threads::Clone(ThreadState* current_state, CloneArgs* args) {
         WARN("Clone with rare flags!!\nSP: %lx, TLS: %lx, Flags: %s", args->new_rsp, args->new_tls, sflags.c_str());
     }
 
-    u64 allowed_flags = CLONE_VM | CLONE_THREAD | CLONE_SYSVSEM | CLONE_CHILD_CLEARTID | CLONE_CHILD_SETTID | CLONE_SIGHAND | CLONE_FILES | CLONE_FS |
-                        CLONE_IO | CLONE_SETTLS | CLONE_PARENT_SETTID | CLONE_VFORK | CLONE_UNTRACED;
+    u64 allowed_flags = CLONE_VM | CLONE_THREAD | CLONE_DETACHED | CLONE_SYSVSEM | CLONE_CHILD_CLEARTID | CLONE_CHILD_SETTID | CLONE_SIGHAND |
+                        CLONE_FILES | CLONE_FS | CLONE_IO | CLONE_SETTLS | CLONE_PARENT_SETTID | CLONE_VFORK | CLONE_UNTRACED;
     if ((args->guest_flags & ~CSIGNAL) & ~allowed_flags) {
         ERROR("Unsupported flags %s", sflags.c_str());
         return -ENOSYS;
