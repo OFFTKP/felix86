2e866c1c800e17a72faaa8d84d8efe4193ec4b90
offtkp <parisoplop@gmail.com>
2025-06-28 16:56:26 +0300

    .

--- src/felix86/common/global.cpp
+++ src/felix86/common/global.cpp
@@ -332,8 +332,8 @@ void initialize_globals() {
                     ERROR("Path returned by felix86-mounter does not exist: %s", path.c_str());
                 }
             } else {
-                printf("The error is hit\n");
-                ERROR("felix86-mounter failed!!!\nError message:\n\n%s", buffer);
+                printf("felix86-mounter failed!!!\nError message:\n\n%s", buffer);
+                exit(1);
             }
         } else {
             ERROR("felix86-mounter didn't exit normally?");
