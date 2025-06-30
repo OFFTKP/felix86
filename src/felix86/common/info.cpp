2555c62fe217428630286dee3a9b4520d88294d2
offtkp <parisoplop@gmail.com>
2025-07-01 01:30:47 +0300

    Update version and readme

--- src/felix86/common/info.cpp
+++ src/felix86/common/info.cpp
@@ -3,7 +3,7 @@
 extern const char* g_git_hash;
 
 #define YEAR "25"
-#define MONTH "07"
+#define MONTH "08"
 
 const char* get_version_full() {
     static std::string version = "felix86 " YEAR "." MONTH + (std::string(g_git_hash) == "?" ? "" : " (" + std::string(g_git_hash) + ")");
