e2497a5b10e4b372c219dc4bbfa4f6671e64ca00
offtkp <parisoplop@gmail.com>
2025-06-30 03:58:15 +0300

    .

--- src/felix86/common/global.cpp
+++ src/felix86/common/global.cpp
@@ -324,7 +324,7 @@ void initialize_globals() {
                 std::filesystem::path path = buffer;
                 if (std::filesystem::exists(path)) {
                     // Relocate executable path
-                    std::string new_executable_path = g_params.executable_path;
+                    std::string new_executable_path = std::filesystem::absolute(g_params.executable_path);
                     Filesystem::removeRootfsPrefix(new_executable_path);
                     new_executable_path = path / std::filesystem::path(new_executable_path).relative_path();
                     g_params.executable_path = new_executable_path;
