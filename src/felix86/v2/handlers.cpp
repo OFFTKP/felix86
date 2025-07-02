f28a485003311cbfd3b0e8e7a2084f0a62b22406
offtkp <parisoplop@gmail.com>
2025-07-02 21:15:06 +0300

    .

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -401,7 +401,7 @@ FAST_HANDLE(ADD) {
     biscuit::GPR dst;
 
     bool writeback = true;
-    bool needs_atomic = operands[0].type == ZYDIS_OPERAND_TYPE_MEMORY && (instruction.attributes & ZYDIS_ATTRIB_HAS_LOCK);
+    bool needs_atomic = true || operands[0].type == ZYDIS_OPERAND_TYPE_MEMORY && (instruction.attributes & ZYDIS_ATTRIB_HAS_LOCK);
     if (needs_atomic) {
         biscuit::GPR address = rec.lea(&operands[0]);
         dst = rec.scratch();
@@ -590,19 +590,120 @@ FAST_HANDLE(SUB) {
     biscuit::GPR dst;
 
     bool writeback = true;
-    bool needs_atomic = operands[0].type == ZYDIS_OPERAND_TYPE_MEMORY && (instruction.attributes & ZYDIS_ATTRIB_HAS_LOCK);
-    bool too_small_for_atomic = operands[0].size == 8 || operands[0].size == 16;
-    if (needs_atomic && !too_small_for_atomic) {
+    bool needs_atomic = true || operands[0].type == ZYDIS_OPERAND_TYPE_MEMORY && (instruction.attributes & ZYDIS_ATTRIB_HAS_LOCK);
+    if (needs_atomic) {
         biscuit::GPR address = rec.lea(&operands[0]);
         dst = rec.scratch();
-        biscuit::GPR src_neg = rec.scratch();
-        as.NEG(src_neg, src);
         switch (operands[0].size) {
+        case 8: {
+            /*
+                andi    a2, a0, -4
+                slli    a0, a0, 3
+                li      a3, 255
+                sllw    a3, a3, a0
+                sllw    a0, a1, a0
+            .LBB0_1:
+                lr.w.aqrl       a1, (a2)
+                sub     a4, a1, a0
+                xor     a4, a4, a1
+                and     a4, a4, a3
+                xor     a4, a4, a1
+                sc.w.rl a4, a4, (a2)
+                bnez    a4, .LBB0_1
+            */
+            biscuit::Label loop;
+            biscuit::GPR masked_address = rec.scratch();
+            biscuit::GPR mask = rec.scratch();
+            as.ANDI(masked_address, address, -4);
+            as.SLLI(address, address, 3);
+            as.LI(mask, 0xFF);
+            as.SLLW(mask, mask, address);
+            as.SLLW(address, src, address);
+
+            as.Bind(&loop);
+            as.LR_W(Ordering::AQRL, dst, masked_address);
+            as.SUB(result, dst, address);
+            as.XOR(result, result, dst);
+            as.AND(result, result, mask);
+            as.XOR(result, result, dst);
+            as.SC_W(Ordering::AQRL, result, result, masked_address);
+            as.BNEZ(result, &loop);
+
+            as.SRLW(dst, dst, address);
+            as.ANDI(dst, dst, 0xFF);
+
+            rec.popScratch();
+            rec.popScratch();
+            break;
+        }
+        case 16: {
+            /*
+            aadd(unsigned short*, unsigned short)
+                andi    a6, a0, -4
+                slli    a0, a0, 3
+                lui     a3, 16
+                addi    a3, a3, -1
+                sllw    a4, a3, a0
+                sllw    a1, a1, a0
+            .LBB0_3:
+                lr.w.aqrl       a5, (a6)
+                sub     a2, a5, a1
+                xor     a2, a2, a5
+                and     a2, a2, a4
+                xor     a2, a2, a5
+                sc.w.rl a2, a2, (a6)
+                bnez    a2, .LBB0_3
+                srlw    a0, a5, a0
+                and     a0, a0, a3
+            */
+            biscuit::Label loop, bad_alignment, end;
+            biscuit::GPR masked_address = rec.scratch();
+            biscuit::GPR mask = rec.scratch();
+            as.LI(mask, 0b11);
+            as.ANDI(masked_address, address, 0b11);
+            as.BEQ(masked_address, mask, &bad_alignment);
+
+            biscuit::GPR s_a1 = rec.scratch();
+            biscuit::GPR s_a3 = mask;
+            biscuit::GPR s_a2 = rec.flag(X86_REF_CF); // ran out of scratch and these get modified later
+            biscuit::GPR s_a4 = rec.flag(X86_REF_SF);
+            biscuit::GPR s_a5 = rec.flag(X86_REF_ZF);
+            biscuit::GPR s_a6 = masked_address;
+            as.ANDI(s_a6, address, -4);
+            as.SLLI(address, address, 3);
+            as.LI(s_a3, 0xFFFF);
+            as.SLLW(s_a4, s_a3, address);
+            as.SLLW(s_a1, src, address);
+            as.Bind(&loop);
+            as.LR_W(Ordering::AQRL, s_a5, s_a6);
+            as.SUB(s_a2, s_a5, s_a1);
+            as.XOR(s_a2, s_a2, s_a5);
+            as.AND(s_a2, s_a2, s_a4);
+            as.XOR(s_a2, s_a2, s_a5);
+            as.SC_W(Ordering::AQRL, s_a2, s_a2, s_a6);
+            as.BNEZ(s_a2, &loop);
+            as.SRLW(dst, s_a5, address);
+            as.AND(dst, dst, s_a3);
+
+            as.J(&end);
+            as.Bind(&bad_alignment);
+            as.EBREAK();
+
+            as.Bind(&end);
+            rec.popScratch();
+            rec.popScratch();
+            rec.popScratch();
+            break;
+        }
         case 32: {
+            biscuit::GPR src_neg = rec.scratch();
+            as.NEG(src_neg, src);
             as.AMOADD_W(Ordering::AQRL, dst, src_neg, address);
             break;
         }
         case 64: {
+            biscuit::GPR src_neg = rec.scratch();
+            as.NEG(src_neg, src);
             as.AMOADD_D(Ordering::AQRL, dst, src_neg, address);
             break;
         }
