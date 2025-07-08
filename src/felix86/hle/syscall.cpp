4a481f83b93dc2c052d738dd5d4e3b6b37bc800d
offtkp <parisoplop@gmail.com>
2025-07-08 13:39:16 +0300

    Implement tkill

--- src/felix86/hle/syscall.cpp
+++ src/felix86/hle/syscall.cpp
@@ -708,6 +708,11 @@ Result felix86_syscall_common(felix86_frame* frame, int rv_syscall, u64 arg1, u6
         result = SYSCALL(tgkill, arg1, arg2, arg3, arg4, arg5, arg6);
         break;
     }
+    case felix86_riscv64_tkill: {
+        SIGLOG("Calling tkill with sig: %d for TID: %d", arg2, arg1);
+        result = SYSCALL(tkill, arg1, arg2);
+        break;
+    }
     case felix86_riscv64_kill: {
         SIGLOG("Calling kill with sig: %d and PID: %d", arg2, arg1);
         result = SYSCALL(kill, arg1, arg2, arg3, arg4, arg5, arg6);
