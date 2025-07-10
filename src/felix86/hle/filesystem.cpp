73c0d417e5efbd48afc332cf649115b2d02f0d93
offtkp <parisoplop@gmail.com>
2025-07-10 15:48:07 +0300

    Update allowed flags

--- src/felix86/hle/filesystem.cpp
+++ src/felix86/hle/filesystem.cpp
@@ -455,6 +455,7 @@ int Filesystem::Rmdir(const char* dir) {
 }
 
 int Filesystem::Chroot(const char* path) {
+    WARN("chroot(%s)", path);
     // First, do a no-op chroot to check if we have permissions at all
     int result = ::chroot("/");
     if (result != 0) {
--- src/felix86/hle/thread.cpp
+++ src/felix86/hle/thread.cpp
@@ -319,7 +319,7 @@ long Threads::Clone(ThreadState* current_state, CloneArgs* args) {
 
     u64 allowed_flags = CLONE_VM | CLONE_THREAD | CLONE_DETACHED | CLONE_SYSVSEM | CLONE_CHILD_CLEARTID | CLONE_CHILD_SETTID | CLONE_SIGHAND |
                         CLONE_FILES | CLONE_FS | CLONE_IO | CLONE_SETTLS | CLONE_PARENT_SETTID | CLONE_VFORK | CLONE_UNTRACED | CLONE_NEWNS |
-                        CLONE_NEWUSER;
+                        CLONE_NEWUSER | CLONE_NEWUTS | CLONE_NEWNET | CLONE_NEWPID;
     if ((args->guest_flags & ~CSIGNAL) & ~allowed_flags) {
         ERROR("Unsupported flags %s", sflags.c_str());
         return -ENOSYS;
