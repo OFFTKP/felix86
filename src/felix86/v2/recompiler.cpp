683987d015b8ac6eaad21aa1fab0ccfeb1dc9e2a
offtkp <parisoplop@gmail.com>
2025-07-18 15:44:51 +0300

    .

--- src/felix86/v2/recompiler.cpp
+++ src/felix86/v2/recompiler.cpp
@@ -2079,40 +2079,40 @@ void Recompiler::updateOverflowSub(biscuit::GPR lhs, biscuit::GPR rhs, biscuit::
 }
 
 void Recompiler::updateOverflowAdd(biscuit::GPR lhs, biscuit::GPR rhs, biscuit::GPR result, x86_size_e size_e) {
-    // biscuit::GPR of = flag(X86_REF_OF);
-    // biscuit::GPR temp = scratch();
-    // biscuit::GPR lhs_e, rhs_e;
-    // if (size_e == X86_SIZE_BYTE_HIGH) {
-    //     size_e = X86_SIZE_BYTE;
-    // }
-    // if (size_e != X86_SIZE_QWORD) {
-    //     sext(of, rhs, size_e);
-    //     sext(temp, lhs, size_e);
-    //     sext(result, result, size_e);
-    //     lhs_e = temp;
-    //     rhs_e = of;
-    // } else {
-    //     lhs_e = lhs;
-    //     rhs_e = rhs;
-    // }
-    // as.SLT(temp, result, lhs_e);
-    // as.SLTI(of, rhs_e, 0);
-    // as.XOR(temp, temp, of);
-    // as.SNEZ(of, temp);
-    // popScratch();
-    int size = getBitSize(size_e);
     biscuit::GPR of = flag(X86_REF_OF);
     biscuit::GPR temp = scratch();
-    as.OR(of, lhs, rhs);
-    as.NOT(temp, result);
-    as.AND(of, temp, of);
-    as.AND(temp, lhs, rhs);
-    as.OR(of, of, temp);
-    as.SRLI(temp, of, size - 2);
-    as.SRLI(of, of, size - 1);
-    as.XOR(of, of, temp);
-    as.ANDI(of, of, 1);
+    biscuit::GPR lhs_e, rhs_e;
+    if (size_e == X86_SIZE_BYTE_HIGH) {
+        size_e = X86_SIZE_BYTE;
+    }
+    if (size_e != X86_SIZE_QWORD) {
+        sext(of, rhs, size_e);
+        sext(temp, lhs, size_e);
+        sext(result, result, size_e);
+        lhs_e = temp;
+        rhs_e = of;
+    } else {
+        lhs_e = lhs;
+        rhs_e = rhs;
+    }
+    as.SLT(temp, result, lhs_e);
+    as.SLTI(of, rhs_e, 0);
+    as.XOR(temp, temp, of);
+    as.SNEZ(of, temp);
     popScratch();
+    // int size = getBitSize(size_e);
+    // biscuit::GPR of = flag(X86_REF_OF);
+    // biscuit::GPR temp = scratch();
+    // as.OR(of, lhs, rhs);
+    // as.NOT(temp, result);
+    // as.AND(of, temp, of);
+    // as.AND(temp, lhs, rhs);
+    // as.OR(of, of, temp);
+    // as.SRLI(temp, of, size - 2);
+    // as.SRLI(of, of, size - 1);
+    // as.XOR(of, of, temp);
+    // as.ANDI(of, of, 1);
+    // popScratch();
 }
 
 void Recompiler::updateAuxiliaryAdd(biscuit::GPR lhs, biscuit::GPR result) {
