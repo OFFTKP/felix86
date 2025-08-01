49e09ec65443c4fd76c90677f561cf2a491d67df
offtkp <parisoplop@gmail.com>
2025-08-02 02:02:56 +0300

    .

--- src/felix86/hle/filesystem.cpp
+++ src/felix86/hle/filesystem.cpp
@@ -91,14 +91,11 @@ int Filesystem::OpenAt(int fd, const char* filename, int flags, u64 mode) {
     }
 
     if (!g_mode32) {
-        if (fd == AT_FDCWD && filename && filename[0] == '/') {
-            // TODO: support fd+path too
-            // We may be opening a library, check if it's one of our overlays
-            const char* overlay = Overlays::isOverlay(filename);
-            if (overlay) {
-                // Open the overlayed path instead of filename
-                return openatInternal(AT_FDCWD, overlay, flags, mode);
-            }
+        // We may be opening a library, check if it's one of our overlays
+        const char* overlay = Overlays::isOverlay(fd, filename);
+        if (overlay) {
+            // Open the overlayed path instead of filename
+            return openatInternal(AT_FDCWD, overlay, flags, mode);
         }
     }
 
