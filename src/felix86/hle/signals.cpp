bcbae327f81276a5fc491a6866496a9105ecf6a9
offtkp <parisoplop@gmail.com>
2025-08-05 18:46:28 +0300

    print smc

--- src/felix86/hle/signals.cpp
+++ src/felix86/hle/signals.cpp
@@ -456,6 +456,7 @@ bool handle_smc(ThreadState* current_state, siginfo_t* info, ucontext_t* context
         return false;
     }
 
+    SMCLOG("Handling SMC on %lx during PC: %lx", info->si_addr, pc);
     u64 write_address = (u64)info->si_addr & ~0xFFFull;
     Recompiler::invalidateRangeGlobal(write_address, write_address + 0x1000);
     ASSERT_MSG(::mprotect((void*)write_address, 0x1000, PROT_READ | PROT_WRITE) == 0, "mprotect failed on address %lx", write_address);
