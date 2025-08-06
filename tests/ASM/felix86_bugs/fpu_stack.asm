1f2723e7dd4e6ef285474723cddccbcb394eeab3
offtkp <parisoplop@gmail.com>
2025-08-07 00:30:15 +0300

    .

--- tests/ASM/felix86_bugs/fpu_stack.asm
+++ tests/ASM/felix86_bugs/fpu_stack.asm
@@ -14,18 +14,12 @@ finit
 lea rdi, [rsp - 512]
 lea rsi, [rsp - 1024]
 lea r8, [rsp - 1024 - 512]
-fxsave [rsp - 8200]
-fxrstor [rsp - 8200]
 fnstenv [rdi]
 mov rax, 0x3ff0000000000000
 mov [rsp - 8], rax
 fld qword [rsp - 8]
-fxsave [rsp - 8200]
-fxrstor [rsp - 8200]
 fnstenv [rsi]
 fstp qword [rsp - 8]
-fxsave [rsp - 8200]
-fxrstor [rsp - 8200]
 fnstenv [r8]
 xor eax, eax
 xor ebx, ebx
