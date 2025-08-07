0624f0264bfca3d9a923f86858382c191fd9bab4
offtkp <parisoplop@gmail.com>
2025-08-07 23:36:24 +0300

    .

--- external/biscuit/include/biscuit/assert.hpp
+++ external/biscuit/include/biscuit/assert.hpp
@@ -21,14 +21,12 @@
 #include <cstdio>
 #include <cstdlib>
 
-#define BISCUIT_ASSERT(condition)                                        \
-  do {                                                                   \
-    if (!(condition)) {                                                  \
-      std::printf("Assertion failed (%s)\nin %s, function %s line %i\n", \
-                  #condition,                                            \
-                  __FILE__, __func__, __LINE__);                         \
-      std::abort();                                                      \
-    }                                                                    \
-  } while (false)
+#define BISCUIT_ASSERT(condition)                                                                                                                    \
+    do {                                                                                                                                             \
+        if (!(condition)) {                                                                                                                          \
+            std::printf("Assertion failed (%s)\nin %s, function %s line %i\n", #condition, __FILE__, __func__, __LINE__);                            \
+            sleep(500);                                                                                                                              \
+        }                                                                                                                                            \
+    } while (false)
 
 #endif
