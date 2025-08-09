0b96b9eacb6073155e27aa567edde9c4df092691
offtkp <parisoplop@gmail.com>
2025-08-09 19:30:01 +0300

    .

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -5325,7 +5325,7 @@ void ROUND(Recompiler& rec, u64 rip, Assembler& as, ZydisDecodedInstruction& ins
 
     // There's sign differences when rounding towards zero. For example, round(-0.5) becomes -0.0 in x86, 0.0 in RISC-V
     // So we restore the sign bit after rounding
-    as.VFSGNJ(dst, dst, sign_bit);
+    as.VFSGNJ(dst, sign_bit, dst);
 
     rec.setVec(&operands[0], dst);
     if (!dyn_round) {
