d6eb1541b228054a7c534e7a9fd654777ba46837
offtkp <parisoplop@gmail.com>
2025-12-01 21:01:38 +0200

    .

--- src/felix86/hle/syscall.cpp
+++ src/felix86/hle/syscall.cpp
@@ -1554,7 +1554,7 @@ Result felix86_syscall_common(felix86_frame* frame, int rv_syscall, u64 arg1, u6
 
         syscall(SYS_execve, executable.c_str(), &argv[0], envp.data());
 
-        ASSERT_MSG(false, "Error during execve: %s", strerror(errno));
+        ASSERT_MSG(false, "Error during execve: %s (executable: %s)", strerror(errno), executable.c_str());
         break;
     }
     case felix86_riscv64_umask: {
