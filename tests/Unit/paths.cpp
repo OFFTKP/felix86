ac06eaa3e7f31c09d9c9e2c5f6747d0a4a959cb9
offtkp <parisoplop@gmail.com>
2025-12-02 20:05:10 +0200

    .

--- tests/Unit/paths.cpp
+++ tests/Unit/paths.cpp
@@ -176,7 +176,7 @@ CATCH_TEST_CASE("Trusted escape", "[paths]") {
     PROLOGUE();
 
     g_fake_mounts.clear();
-    CATCH_REQUIRE(Filesystem::FakeMount("/run", g_original_rootfs / "run"));
+    CATCH_REQUIRE(Filesystem::FakeMount("/run", g_config.rootfs_path / "run"));
     std::filesystem::path trusted_dir = std::filesystem::path("/tmp/felix86_paths") / "trusted";
     std::filesystem::create_directories(trusted_dir / "subdir1" / "subdir2");
     CATCH_REQUIRE(Filesystem::TrustFolder(trusted_dir));
@@ -201,7 +201,7 @@ CATCH_TEST_CASE("Trusted escape 2", "[paths]") {
     PROLOGUE();
 
     g_fake_mounts.clear();
-    CATCH_REQUIRE(Filesystem::FakeMount("/run", g_original_rootfs / "run"));
+    CATCH_REQUIRE(Filesystem::FakeMount("/run", g_config.rootfs_path / "run"));
     std::filesystem::path trusted_dir = std::filesystem::path("/tmp/felix86_paths") / "trusted";
     std::filesystem::create_directories(trusted_dir / "subdir1" / "subdir2");
     CATCH_REQUIRE(Filesystem::TrustFolder(trusted_dir));
@@ -221,13 +221,13 @@ CATCH_TEST_CASE("Trusted escape 2", "[paths]") {
     CATCH_REQUIRE(!resolved3.is_error());
 
     // TODO: remove trailing slash
-    CATCH_REQUIRE(resolved3.full_path() == g_original_rootfs / "");
+    CATCH_REQUIRE(resolved3.full_path() == g_config.rootfs_path / "");
 
     FdPath resolved4 = Filesystem::resolve((node.dst_path / "subdir1" / ".." / ".." / ".." / ".." / ".." / ".." / ".." / ".." / "..").c_str(), true);
     CATCH_REQUIRE(!resolved4.is_error());
 
     // TODO: remove trailing slash
-    CATCH_REQUIRE(resolved4.full_path() == g_original_rootfs / "");
+    CATCH_REQUIRE(resolved4.full_path() == g_config.rootfs_path / "");
 
     g_fake_mounts.clear();
 
@@ -263,7 +263,7 @@ CATCH_TEST_CASE("Escape root", "[paths]") {
 
     {
         g_fake_mounts.clear();
-        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_original_rootfs / "proc"));
+        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_config.rootfs_path / "proc"));
         FdPath resolved = Filesystem::resolve("/proc", true);
         CATCH_REQUIRE(!resolved.is_error());
 
@@ -274,7 +274,7 @@ CATCH_TEST_CASE("Escape root", "[paths]") {
 
     {
         g_fake_mounts.clear();
-        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_original_rootfs / "proc"));
+        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_config.rootfs_path / "proc"));
         FdPath resolved = Filesystem::resolve("/proc/..", true);
         CATCH_REQUIRE(!resolved.is_error());
 
@@ -285,7 +285,7 @@ CATCH_TEST_CASE("Escape root", "[paths]") {
 
     {
         g_fake_mounts.clear();
-        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_original_rootfs / "proc"));
+        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_config.rootfs_path / "proc"));
         FdPath resolved = Filesystem::resolve("/proc/../../../..", true);
         CATCH_REQUIRE(!resolved.is_error());
 
@@ -296,7 +296,7 @@ CATCH_TEST_CASE("Escape root", "[paths]") {
 
     {
         g_fake_mounts.clear();
-        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_original_rootfs / "proc"));
+        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_config.rootfs_path / "proc"));
         FdPath resolved = Filesystem::resolve("/proc/../proc/../..", true);
         CATCH_REQUIRE(!resolved.is_error());
 
@@ -314,7 +314,7 @@ CATCH_TEST_CASE("Proc self ns user", "[paths]") {
     {
         g_fake_mounts.clear();
         g_config.verbose = 1;
-        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_original_rootfs / "proc"));
+        CATCH_REQUIRE(Filesystem::FakeMount("/proc", g_config.rootfs_path / "proc"));
         FdPath resolved = Filesystem::resolve("/proc/self/ns/user", true);
         CATCH_REQUIRE(!resolved.is_error());
 
