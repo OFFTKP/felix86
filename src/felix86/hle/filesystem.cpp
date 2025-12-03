6b84f0822b63d73ce7872a951effe9b11dfd5f33
offtkp <parisoplop@gmail.com>
2025-12-03 22:59:05 +0200

    .

--- src/felix86/hle/filesystem.cpp
+++ src/felix86/hle/filesystem.cpp
@@ -676,7 +676,7 @@ int Filesystem::Chroot(const char* path) {
     // we will do without
     std::filesystem::path final_path;
     std::filesystem::path dir = create_unique_mount_path();
-    int result = ::mount(fd_path.full_path(), dir.c_str(), nullptr, MS_MOVE, nullptr);
+    int result = ::mount(fd_path.full_path(), dir.c_str(), nullptr, MS_BIND, nullptr);
     if (result == 0) {
         final_path = dir;
     } else {
