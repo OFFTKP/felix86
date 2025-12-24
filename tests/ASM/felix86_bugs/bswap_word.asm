18512747721a7bfef5ea9fd847a46cedf43326cc
Paris Oplopoios <parisoplop@gmail.com>
2025-12-24 21:22:51 +0200

    .

--- tests/ASM/felix86_bugs/bswap_word.asm
+++ tests/ASM/felix86_bugs/bswap_word.asm
@@ -1,13 +1,13 @@
 %ifdef CONFIG
 {
   "RegData": {
-    "RAX": "0x1234567890abefcd",
+    "RAX": "0x1234567890abefcd"
   }
 }
 %endif
 bits 64
 
 mov rax, 0x1234567890abcdef
-bswap ax
+db 0x66, 0x0f, 0xc8
 
 hlt
