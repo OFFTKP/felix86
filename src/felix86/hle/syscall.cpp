b43cdc0f53bb65cc92f607ea1c7dffce3b1a0694
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-01-01 22:32:14 +0200

    Implement keyctl & co

--- src/felix86/hle/syscall.cpp
+++ src/felix86/hle/syscall.cpp
@@ -327,6 +327,18 @@ Result felix86_syscall_common(felix86_frame* frame, int rv_syscall, u64 arg1, u6
         result = SYSCALL(getsid, arg1);
         break;
     }
+    case felix86_riscv64_keyctl: {
+        result = SYSCALL(keyctl, arg1, arg2, arg3, arg4, arg5);
+        break;
+    }
+    case felix86_riscv64_add_key: {
+        result = SYSCALL(add_key, arg1, arg2, arg3, arg4, arg5);
+        break;
+    }
+    case felix86_riscv64_request_key: {
+        result = SYSCALL(request_key, arg1, arg2, arg3, arg4);
+        break;
+    }
     case felix86_riscv64_bind: {
         // TODO: HACK: does this need porting to other socket related syscalls?
         auto sock = (sockaddr*)arg2;
