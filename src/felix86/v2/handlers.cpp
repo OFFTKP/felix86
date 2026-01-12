141ffe84b18f4d1705cc65dd164dfdded31e7f7e
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-01-12 13:06:54 +0200

    .

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -4554,14 +4554,17 @@ FAST_HANDLE(PMULLD) {
 }
 
 FAST_HANDLE(PMULUDQ) {
-    biscuit::Vec result = rec.scratchVecM2();
     biscuit::Vec dst = rec.getVec(&operands[0]);
     biscuit::Vec src = rec.getVec(&operands[1]);
+    biscuit::Vec dst_masked = rec.scratchVec();
+    biscuit::Vec src_masked = rec.scratchVec();
 
-    rec.setVectorState(SEW::E32, 4);
-    as.VWMULU(result, dst, src);
+    rec.setVectorState(SEW::E64, 2);
+    as.VZEXTVF2(dst_masked, dst);
+    as.VZEXTVF2(src_masked, src);
+    as.VMUL(dst, dst_masked, src_masked);
 
-    rec.setVec(&operands[0], result);
+    rec.setVec(&operands[0], dst);
 }
 
 FAST_HANDLE(PMULDQ) {
