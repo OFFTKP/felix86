0d601ba79dc20648a3f27aa067822756c2ce1629
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-02-08 17:17:46 +0200

    Stuff

---
+++ src/felix86/hle/seccomp.cpp
@@ -0,0 +1,375 @@
+#include <cstddef>
+#include <linux/audit.h>
+#include <linux/bpf_common.h>
+#include "biscuit/assembler.hpp"
+#include "felix86/common/global.hpp"
+#include "felix86/common/log.hpp"
+#include "felix86/common/state.hpp"
+#include "felix86/hle/seccomp.hpp"
+#include "felix86/v2/recompiler.hpp"
+
+static u64 g_filter_index = 0;
+static std::vector<u8> g_filter_instructions{};
+
+struct x64_sock_filter {
+    u16 code;
+    u8 jt;
+    u8 jf;
+    u32 k;
+};
+
+struct x64_sock_fprog {
+    u16 len;
+    const x64_sock_filter* array;
+};
+
+struct x64_seccomp_data {
+    u32 nr;
+    u32 arch;
+    u64 rip;
+    u64 args[6];
+};
+
+struct BPFJit {
+    explicit BPFJit(Assembler& as, u64 rip) : as(as), rip(rip) {}
+    ~BPFJit() = default;
+
+    void compileProgram(const x64_sock_fprog* program) {
+        ASSERT(!g_mode32);
+        prologue();
+        for (int i = 0; i < program->len; i++) {
+            compileInstruction(program->array[i]);
+        }
+        epilogue();
+    }
+
+    void printProgram(const x64_sock_fprog* program) {
+        for (int i = 0; i < program->len; i++) {
+            printInstruction(program->array[i]);
+        }
+    }
+
+private:
+    BPFJit(const BPFJit&) = delete;
+    BPFJit& operator=(const BPFJit&) = delete;
+    BPFJit(BPFJit&&) = delete;
+    BPFJit& operator=(BPFJit&&) = delete;
+    void prologue();
+    void compileInstruction(const x64_sock_filter& instruction);
+    void printInstruction(const x64_sock_filter& instruction);
+    void epilogue();
+
+    Assembler& as;
+    u64 rip;
+};
+
+// Uses a specific convention to match the scratch registers in the felix86 recompiler:
+// X -> x28
+// A -> x29
+// temporary -> x30
+// pointer to data -> x31
+constexpr inline auto X = x28;
+constexpr inline auto A = x29;
+constexpr inline auto temp = x30;
+constexpr inline auto pointer = x31;
+void BPFJit::compileInstruction(const x64_sock_filter& instruction) {
+    static_assert(Recompiler::isScratch(x28));
+    static_assert(Recompiler::isScratch(x29));
+    static_assert(Recompiler::isScratch(x30));
+    static_assert(Recompiler::isScratch(x31));
+    u16 code = instruction.code;
+#define SRC() (BPF_SRC(code) == BPF_K ? temp : X)
+    switch (BPF_CLASS(code)) {
+    case BPF_LD: {
+        ASSERT(BPF_SIZE(code) == BPF_W);
+        switch (BPF_MODE(code)) {
+        case BPF_IMM: {
+            break;
+        }
+        case BPF_ABS: {
+            break;
+        }
+        case BPF_LEN: {
+            break;
+        }
+        case BPF_MEM: {
+            break;
+        }
+        default: {
+            ERROR("Bad BPF mode: %x", BPF_MODE(code));
+            break;
+        }
+        }
+        break;
+    }
+    case BPF_ALU: {
+        if (BPF_SRC(code) == BPF_K) {
+            as.LI(temp, instruction.k);
+        }
+        switch (BPF_OP(code)) {
+        case BPF_ADD: {
+            as.ADDW(A, A, SRC());
+            break;
+        }
+        case BPF_SUB: {
+            as.SUBW(A, A, SRC());
+            break;
+        }
+        case BPF_MUL: {
+            as.MULW(A, A, SRC());
+            break;
+        }
+        case BPF_DIV: {
+            biscuit::Label is_zero, end;
+            as.BEQZ(SRC(), &is_zero);
+            as.DIVW(A, A, SRC());
+            as.J(&end);
+            as.Bind(&is_zero);
+            as.MV(A, x0);
+            as.Bind(&end);
+            break;
+        }
+        case BPF_OR: {
+            as.OR(A, A, SRC());
+            break;
+        }
+        case BPF_AND: {
+            as.AND(A, A, SRC());
+            break;
+        }
+        case BPF_LSH: {
+            as.SLLW(A, A, SRC());
+            break;
+        }
+        case BPF_RSH: {
+            as.SRLW(A, A, SRC());
+            break;
+        }
+        case BPF_NEG: {
+            as.NEGW(A, A);
+            break;
+        }
+        case BPF_XOR: {
+            as.XOR(A, A, SRC());
+            break;
+        }
+        default: {
+            ERROR("Bad BPF operation: %x", BPF_OP(code));
+            break;
+        }
+        }
+    }
+    }
+#undef SRC
+}
+
+void BPFJit::printInstruction(const x64_sock_filter& instruction) {
+    u16 code = instruction.code;
+    std::string cl, size, mode, op, opj, src;
+    switch (BPF_CLASS(code)) {
+    case BPF_LD:
+        cl = "BPF_LD";
+        break;
+    case BPF_LDX:
+        cl = "BPF_LDX";
+        break;
+    case BPF_ST:
+        cl = "BPF_ST";
+        break;
+    case BPF_STX:
+        cl = "BPF_STX";
+        break;
+    case BPF_ALU:
+        cl = "BPF_ALU";
+        break;
+    case BPF_JMP:
+        cl = "BPF_JMP";
+        break;
+    case BPF_RET:
+        cl = "BPF_RET";
+        break;
+    case BPF_MISC:
+        cl = "BPF_MISC";
+        break;
+    default:
+        UNREACHABLE();
+    }
+    switch (BPF_SIZE(code)) {
+    case BPF_B: {
+        size = "BPF_B";
+        break;
+    }
+    case BPF_H: {
+        size = "BPF_H";
+        break;
+    }
+    case BPF_W: {
+        size = "BPF_W";
+        break;
+    }
+    default:
+        UNREACHABLE();
+    }
+    switch (BPF_MODE(code)) {
+    case BPF_IMM:
+        mode = "BPF_IMM";
+        break;
+    case BPF_ABS:
+        mode = "BPF_ABS";
+        break;
+    case BPF_IND:
+        mode = "BPF_IND";
+        break;
+    case BPF_MEM:
+        mode = "BPF_MEM";
+        break;
+    case BPF_LEN:
+        mode = "BPF_LEN";
+        break;
+    case BPF_MSH:
+        mode = "BPF_MSH";
+        break;
+    default:
+        UNREACHABLE();
+    }
+    switch (BPF_OP(code)) {
+    case BPF_ADD:
+        op = "BPF_ADD";
+        break;
+    case BPF_SUB:
+        op = "BPF_SUB";
+        break;
+    case BPF_MUL:
+        op = "BPF_MUL";
+        break;
+    case BPF_DIV:
+        op = "BPF_DIV";
+        break;
+    case BPF_OR:
+        op = "BPF_OR";
+        break;
+    case BPF_AND:
+        op = "BPF_AND";
+        break;
+    case BPF_LSH:
+        op = "BPF_LSH";
+        break;
+    case BPF_RSH:
+        op = "BPF_RSH";
+        break;
+    case BPF_NEG:
+        op = "BPF_NEG";
+        break;
+    case BPF_MOD:
+        op = "BPF_MOD";
+        break;
+    case BPF_XOR:
+        op = "BPF_XOR";
+        break;
+    default:
+        UNREACHABLE();
+    }
+    switch (BPF_OP(code)) {
+    case BPF_JA:
+        opj = "BPF_JA";
+        break;
+    case BPF_JEQ:
+        opj = "BPF_JEQ";
+        break;
+    case BPF_JGT:
+        opj = "BPF_JGT";
+        break;
+    case BPF_JGE:
+        opj = "BPF_JGE";
+        break;
+    case BPF_JSET:
+        opj = "BPF_JSET";
+        break;
+    default:
+        UNREACHABLE();
+    }
+    switch (BPF_SRC(code)) {
+    case BPF_K:
+        src = "BPF_K";
+        break;
+    case BPF_X:
+        src = "BPF_X";
+        break;
+    default:
+        UNREACHABLE();
+    }
+
+    switch (BPF_CLASS(code)) {
+    case BPF_LD:
+    case BPF_ST:
+    case BPF_LDX:
+    case BPF_STX:
+        PLAIN("%s | %s | %s", cl.c_str(), size.c_str(), mode.c_str());
+        break;
+    case BPF_ALU:
+        if (BPF_SRC(code) == BPF_K) {
+            PLAIN("%s | %s | %s | %s (k: %x)", cl.c_str(), size.c_str(), op.c_str(), src.c_str(), instruction.k);
+        } else {
+            PLAIN("%s | %s | %s | %s", cl.c_str(), size.c_str(), op.c_str(), src.c_str());
+        }
+        break;
+    case BPF_JMP:
+        if (BPF_SRC(code) == BPF_K) {
+            PLAIN("%s | %s | %s | %s (k: %x)", cl.c_str(), size.c_str(), opj.c_str(), src.c_str(), instruction.k);
+        } else {
+            PLAIN("%s | %s | %s | %s", cl.c_str(), size.c_str(), opj.c_str(), src.c_str());
+        }
+        break;
+    case BPF_RET:
+        if (BPF_SRC(code) == BPF_K) {
+            PLAIN("%s | %s (k: %x)", cl.c_str(), src.c_str(), instruction.k);
+        } else {
+            PLAIN("%s | %s", cl.c_str(), src.c_str());
+        }
+        break;
+    case BPF_MISC:
+        PLAIN("BPF_MISC...?");
+        break;
+    default:
+        UNREACHABLE();
+    }
+}
+
+void BPFJit::prologue() {
+    as.ADDI(sp, sp, (i32)(-sizeof(x64_seccomp_data)));
+    as.MV(pointer, sp);
+
+    as.SW(Recompiler::allocatedGPR(X86_REF_RAX), offsetof(x64_seccomp_data, nr), pointer);
+
+    as.LI(temp, AUDIT_ARCH_X86_64);
+    as.SW(temp, offsetof(x64_seccomp_data, arch), pointer);
+
+    as.LI(temp, rip);
+    as.SD(temp, offsetof(x64_seccomp_data, rip), pointer);
+
+    int i = 0;
+    auto refs = {X86_REF_RDI, X86_REF_RSI, X86_REF_RDX, X86_REF_R10, X86_REF_R8, X86_REF_R9};
+    for (auto ref : refs) {
+        as.SD(Recompiler::allocatedGPR(ref), offsetof(x64_seccomp_data, args) + i++ * sizeof(u64), pointer);
+    }
+}
+
+void BPFJit::epilogue() {
+    as.ADDI(sp, sp, sizeof(x64_seccomp_data));
+}
+
+bool Seccomp::setFilter(u32 flags, void* args, u64 rip) {
+    if (flags & ~(0)) {
+        ERROR("Unsupported seccomp flags: %x", flags);
+    }
+
+    if (g_filter_instructions.size() - g_filter_index < 4096) {
+        g_filter_instructions.resize(g_filter_instructions.size() + 4096);
+    }
+
+    u8* pointer = g_filter_instructions.data() + g_filter_index;
+    Assembler as(pointer, g_filter_instructions.size() - g_filter_index);
+    BPFJit jit(as, rip);
+    jit.printProgram((const x64_sock_fprog*)args);
+    return true;
+}
---
+++ src/felix86/hle/seccomp.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "biscuit/assembler.hpp"
+#include "felix86/common/types.hpp"
+
+struct Seccomp {
+    static bool setFilter(u32 flags, void* args, u64 rip);
+    static void emitFilters(biscuit::Assembler& as);
+};
