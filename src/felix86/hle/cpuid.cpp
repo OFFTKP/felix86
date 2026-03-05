f67e42ff12715d83154451d998311ada86472cb9
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-03-05 17:36:39 +0200

    Enable POPCNT

--- src/felix86/hle/cpuid.cpp
+++ src/felix86/hle/cpuid.cpp
@@ -150,6 +150,7 @@ Cpuid felix86_cpuid_impl(u32 leaf, u32 subleaf) {
         bit_set(result.ecx, 9, is_feature_enabled(x86_feature::SSSE3));
         bit_set(result.ecx, 19, is_feature_enabled(x86_feature::SSE4_1));
         bit_set(result.ecx, 20, is_feature_enabled(x86_feature::SSE4_2));
+        bit_set(result.ecx, 23, is_feature_enabled(x86_feature::LZCNT_POPCNT));
         bit_set(result.ecx, 25, is_feature_enabled(x86_feature::AES));
         bit_set(result.ecx, 27, is_feature_enabled(x86_feature::OSXSAVE));
         bit_set(result.ecx, 28, is_feature_enabled(x86_feature::AVX));
@@ -163,6 +164,7 @@ Cpuid felix86_cpuid_impl(u32 leaf, u32 subleaf) {
     }
 
     if (found && leaf == 0x8000'0001) {
+        bit_set(result.ecx, 0, true); // LAHF/SAHF
         bit_set(result.ecx, 5, is_feature_enabled(x86_feature::LZCNT_POPCNT));
     }
 
