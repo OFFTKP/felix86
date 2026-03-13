83d5252b0a7e17e96757b8ff23c054f0e3119409
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-03-13 17:39:26 +0200

    .

--- src/felix86/common/info.cpp
+++ src/felix86/common/info.cpp
@@ -7,6 +7,10 @@ extern const char* g_git_hash;
 #define MONTH "03"
 
 const char* get_version_full() {
+#ifdef FELIX86_MONTHLY_RELEASE
+    static std::string version = "felix86 " YEAR "." MONTH + std::string(" (release)");
+#else
     static std::string version = "felix86 " YEAR "." MONTH + (std::string(g_git_hash) == "?" ? "" : " (" + std::string(g_git_hash) + ")");
+#endif
     return version.c_str();
 }
