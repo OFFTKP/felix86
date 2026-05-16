a0e0e45ae115295afe37f28d27b4b3cea4ca2af1
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-05-16 13:14:35 +0300

    .

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -9042,6 +9042,9 @@ FAST_HANDLE(FXSAVE) {
     rec.writebackState();
     as.MV(a1, address);
     as.MV(a0, rec.threadStatePointer());
+    as.LI(a2, 1);
+    as.LI(a3, 1);
+    as.LI(a4, 1);
     rec.callPointer(offsetof(ThreadState, felix86_fxsave));
     rec.restoreState();
 }
@@ -9051,6 +9054,9 @@ FAST_HANDLE(FXSAVE64) {
     rec.writebackState();
     as.MV(a1, address);
     as.MV(a0, rec.threadStatePointer());
+    as.LI(a2, 1);
+    as.LI(a3, 1);
+    as.LI(a4, 1);
     rec.callPointer(offsetof(ThreadState, felix86_fxsave));
     rec.restoreState();
 }
@@ -9060,7 +9066,9 @@ FAST_HANDLE(FXRSTOR) {
     rec.writebackState();
     as.MV(a1, address);
     as.MV(a0, rec.threadStatePointer());
-    as.LI(a2, 0);
+    as.LI(a2, 1);
+    as.LI(a3, 1);
+    as.LI(a4, 1);
     rec.callPointer(offsetof(ThreadState, felix86_fxrstor));
     rec.restoreState();
 }
@@ -9071,6 +9079,8 @@ FAST_HANDLE(FXRSTOR64) {
     as.MV(a1, address);
     as.MV(a0, rec.threadStatePointer());
     as.LI(a2, 1);
+    as.LI(a3, 1);
+    as.LI(a4, 1);
     rec.callPointer(offsetof(ThreadState, felix86_fxrstor));
     rec.restoreState();
 }
