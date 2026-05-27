02d5976381fa39519d682209d1974c96b1b7a96b
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-05-27 19:15:26 +0300

    .

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -10696,7 +10696,9 @@ FAST_HANDLE(CMPXCHG16B) {
             as.LI(mem1, knuth_hash);
             as.SRLI(mem0, address, 4); // shift out low bits since they are 0 to get a better hash
             as.MULW(mem0, mem0, mem1);
-            as.ANDI(mem0, mem0, (sizeof(g_process_globals.cas128_locks) / sizeof(u32)) - 1);
+            as.ANDI(mem0, mem0, 0xFF);
+            as.SLLI(mem0, mem0, 2);
+            static_assert(sizeof(g_process_globals.cas128_locks) == 256 * sizeof(u32));
             as.ADD(lock_address, lock_address, mem0);
         }
 
