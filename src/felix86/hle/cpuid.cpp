304307b87d0091c9f84f6fc70aa8e76a08082a71
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-06-13 09:55:07 +0300

    .

--- src/felix86/hle/cpuid.cpp
+++ src/felix86/hle/cpuid.cpp
@@ -197,30 +197,6 @@ Cpuid felix86_cpuid_impl(u32 leaf, u32 subleaf) {
     }
 
     if (leaf == 0x0000'0015) {
-#ifndef RISCV_HWPROBE_KEY_TIME_CSR_FREQ
-#define RISCV_HWPROBE_KEY_TIME_CSR_FREQ 8
-#endif
-#ifdef __riscv
-        riscv_hwprobe pairs[] = {
-            {RISCV_HWPROBE_KEY_TIME_CSR_FREQ, 0},
-        };
-
-        int r = syscall(SYS_riscv_hwprobe, pairs, std::size(pairs), 0, nullptr, 0);
-        uint64_t hz = pairs[0].value;
-        if (r == 0) {
-            // TSCFreq = ECX*(EBX/EAX)
-            result.ecx = hz;
-            result.eax = 1;
-            result.ebx = 1;
-            result.edx = 0;
-        } else {
-            WARN("Failed to query RDTIME frequency");
-        }
-        found = true;
-#endif
-    }
-
-    if (leaf == 0x0000'0015) {
 #ifdef __riscv
         riscv_hwprobe pairs[] = {
             {RISCV_HWPROBE_KEY_TIME_CSR_FREQ, 0},
