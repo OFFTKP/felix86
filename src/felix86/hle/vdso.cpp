abdf6227e0fdf5164c447da9984a988f151e5682
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-07-02 22:25:17 +0300

    Build vdso instead of vendoring it, use both HASH and GNU_HASH
    Older gvisor tests depend on the vdso being built with both GNU_HASH
    and HASH. Also build vdso instead of vendoring it.

--- src/felix86/hle/vdso.cpp
+++ src/felix86/hle/vdso.cpp
@@ -1,6 +1,10 @@
-#include "felix86/hle/guest_libs/x64-linux-vdso.h"
 #include "felix86/hle/vdso.hpp"
 
+extern "C" {
+    extern unsigned char x64_linux_vdso_so_1[];
+    extern unsigned int x64_linux_vdso_so_1_len;
+}
+
 std::span<u8> VDSO::getObject64() {
-    return std::span<u8>{(u8*)x64_linux_vdso_so_1, x64_linux_vdso_so_1_size};
+    return std::span<u8>{x64_linux_vdso_so_1, x64_linux_vdso_so_1_len};
 }
