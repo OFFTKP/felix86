066517394281aed580a3e46257a033025351a607
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-07-02 22:51:59 +0300

    .

--- src/felix86/hle/vdso.cpp
+++ src/felix86/hle/vdso.cpp
@@ -1,8 +1,8 @@
 #include "felix86/hle/vdso.hpp"
 
 extern "C" {
-    extern unsigned char x64_linux_vdso_so_1[];
-    extern unsigned int x64_linux_vdso_so_1_len;
+extern unsigned char x64_linux_vdso_so_1[];
+extern unsigned int x64_linux_vdso_so_1_len;
 }
 
 std::span<u8> VDSO::getObject64() {
