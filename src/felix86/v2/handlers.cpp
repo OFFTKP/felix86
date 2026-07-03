85ae44f2cf475b1b2a8380e1bf0a9bc1d1ce73eb
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-07-03 15:05:13 +0300

    .

--- src/felix86/v2/handlers.cpp
+++ src/felix86/v2/handlers.cpp
@@ -1749,6 +1749,38 @@ FAST_HANDLE(UD2) {
 FAST_HANDLE(CALL) {
     rec.pushCalltrace();
 
+    if (operands[0].type == ZYDIS_OPERAND_TYPE_MEMORY) {
+        if (operands[0].size == 64 + 16 || operands[0].size == 32 + 16) {
+            WARN("Far call detected at %lx", rip);
+            bool bit32 = operands[0].size == 32 + 16;
+            biscuit::GPR rsp = rec.getGPR(X86_REF_RSP, rec.stackWidth());
+            as.ADDI(rsp, rsp, -rec.stackPointerSize() * 2);
+            rec.setGPR(X86_REF_RSP, rec.stackWidth(), rsp);
+            biscuit::GPR cs = rec.scratch();
+            biscuit::GPR scratch = rec.scratch();
+            as.LHU(cs, offsetof(ThreadState, ctx.cs), Recompiler::threadStatePointer());
+            biscuit::GPR ripreg = rec.allocatedGPR(X86_REF_RIP);
+            u64 return_address_offset = (rip - rec.getCurrentRipregValue()) + instruction.length;
+            rec.addi(scratch, ripreg, return_address_offset);
+            rec.writeMemory(scratch, rsp, 0, rec.stackWidth());
+            rec.writeMemory(cs, rsp, rec.stackPointerSize(), rec.stackWidth());
+            rec.popScratch();
+
+            biscuit::GPR address = rec.lea(&operands[0]);
+            rec.readMemory(ripreg, address, 0, bit32 ? X86_SIZE_DWORD : X86_SIZE_QWORD);
+            rec.readMemory(cs, address, bit32 ? 4 : 8, X86_SIZE_WORD);
+            rec.writebackState();
+            as.MV(a1, cs);
+            as.LI(a2, ZYDIS_REGISTER_CS);
+            as.MV(a0, rec.threadStatePointer());
+            rec.callPointer(offsetof(ThreadState, felix86_set_segment));
+            rec.restoreState();
+            rec.backToDispatcher();
+            rec.stopCompiling();
+            return;
+        }
+    }
+
     switch (operands[0].type) {
     case ZYDIS_OPERAND_TYPE_REGISTER:
     case ZYDIS_OPERAND_TYPE_MEMORY: {
