656500e09a1c88502b05eaaec7474e886f91228a
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-08-02 15:40:08 +0300

    .

---
+++ tests/ASM/felix86_bugs/rcl_preserve.asm
@@ -0,0 +1,42 @@
+%ifdef CONFIG
+{
+  "RegData": {
+    "R8": "0x0000000000000001",
+    "R9": "0x0000000000000080",
+    "R10": "0x0000000000000805",
+    "R11": "0x0000000000000000"
+  }
+}
+%endif
+bits 64
+
+mov rax, 0xabcdef1234567890
+mov rbx, 0x800000abcdef1234
+
+add rax, rbx
+rcl rax, 3
+pushfq
+pop r8
+and r8, 0x8d5
+
+or rax, rbx
+rcl rax, 0
+pushfq
+pop r9
+and r9, 0x8d5
+
+mov rcx, 5
+xor rax, rbx
+rcl rax, cl
+pushfq
+pop r10
+and r10, 0x8d5
+
+xor ecx, ecx
+xor rax, rbx
+rcl rax, cl
+pushfq
+pop r11
+and r11, 0x8d5
+
+hlt
