789ea7e62fcbb77c49c811a60a38005d4693b391
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-08-09 12:03:21 +0300

    .

--- src/felix86/common/utility.cpp
+++ src/felix86/common/utility.cpp
@@ -201,6 +201,9 @@ u64 sext_if_64(u64 value, u8 size_e) {
 // Flush icache for current core
 void flush_icache(u64 start, u64 end) {
 #if defined(__riscv)
+#ifndef SYS_RISCV_FLUSH_ICACHE_LOCAL
+#define SYS_RISCV_FLUSH_ICACHE_LOCAL 1
+#endif
     __riscv_flush_icache((void*)start, (void*)end, SYS_RISCV_FLUSH_ICACHE_LOCAL);
 #endif
 }
