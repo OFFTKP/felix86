a034fabebc50cc7359b25be10ff454828b83b42b
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-08-19 21:44:43 +0300

    .

--- src/felix86/hle/cpuid.cpp
+++ src/felix86/hle/cpuid.cpp
@@ -259,7 +259,7 @@ Cpuid felix86_cpuid_impl(u32 leaf, u32 subleaf) {
         WARN("Unknown CPUID(%08x, %08x)", leaf, subleaf);
     }
 
-    CPUIDLOG("CPUID %x %x -> %x %x %x %x", leaf, subleaf, result.eax, result.ebx, result.ecx, result.edx);
+    CPUIDLOG("CPUID %08x %08x -> %08x %08x %08x %08x", leaf, subleaf, result.eax, result.ebx, result.ecx, result.edx);
 
     return result;
 }
