12af801496ae2d4886951bb5244c400cbba4de52
Paris Oplopoios <parisoplop@gmail.com>
2026-09-24 16:30:55 +0300

    Recompiler: Optimize MOV+SHUFPS to PSHUFD (#788)
    PSHUFD has way nicer translations to RVV, some of which are not
    immediately visible to equivalent SHUFPS instructions. x86 code will
    choose not to emit PSHUFD even when it could save an instruction, as it
    crosses domains. Since we don't care about that in RISC-V, we want to
    convert those sequences to PSHUFD so they can be translated better.

--- src/felix86/v2/optimizer.cpp
+++ src/felix86/v2/optimizer.cpp
@@ -39,6 +39,63 @@ static bool is_branch_or_memory(u32 instr) {
     }
 }
 
+void Optimizer::zydis_instruction_pass(ZydisBookkeeping& state, ZydisDecodedInstruction& instruction, ZydisDecodedOperand* operands) {
+    const ZydisMnemonic mnemonic = instruction.mnemonic;
+    if (mnemonic == ZYDIS_MNEMONIC_FXRSTOR || mnemonic == ZYDIS_MNEMONIC_FXRSTOR64 || mnemonic == ZYDIS_MNEMONIC_XRSTOR ||
+        mnemonic == ZYDIS_MNEMONIC_XRSTOR64 || mnemonic == ZYDIS_MNEMONIC_VZEROALL) {
+        state.reset();
+        return;
+    }
+
+    // Check all operands instead of just the first, covers pcmpxstrm and vgather
+    for (int i = 0; i < instruction.operand_count; i++) {
+        const bool modified = operands[i].actions & ZYDIS_OPERAND_ACTION_MASK_WRITE;
+        const bool is_reg = operands[i].type == ZYDIS_OPERAND_TYPE_REGISTER;
+        const bool is_xmm = is_reg && operands[i].reg.value >= ZYDIS_REGISTER_XMM0 && operands[i].reg.value <= ZYDIS_REGISTER_XMM15;
+        const bool is_ymm = is_reg && operands[i].reg.value >= ZYDIS_REGISTER_YMM0 && operands[i].reg.value <= ZYDIS_REGISTER_YMM15;
+        if (modified) {
+            if (is_xmm) {
+                const int dst_index = operands[i].reg.value - ZYDIS_REGISTER_XMM0;
+                if ((mnemonic == ZYDIS_MNEMONIC_MOVAPS || mnemonic == ZYDIS_MNEMONIC_MOVAPD || mnemonic == ZYDIS_MNEMONIC_MOVDQA ||
+                     mnemonic == ZYDIS_MNEMONIC_MOVUPS || mnemonic == ZYDIS_MNEMONIC_MOVUPD || mnemonic == ZYDIS_MNEMONIC_MOVDQU) &&
+                    operands[1].type == ZYDIS_OPERAND_TYPE_REGISTER) {
+                    const int src_index = operands[1].reg.value - ZYDIS_REGISTER_XMM0;
+                    state.add_xmm_copy(dst_index, src_index);
+                } else if (mnemonic == ZYDIS_MNEMONIC_SHUFPS && operands[1].type == ZYDIS_OPERAND_TYPE_REGISTER) {
+                    const int src_index = operands[1].reg.value - ZYDIS_REGISTER_XMM0;
+                    const bool same_value = (state.is_xmm_copy(dst_index) && state.get_xmm_copy(dst_index) == src_index) ||
+                                            (state.is_xmm_copy(src_index) && state.get_xmm_copy(src_index) == dst_index) ||
+                                            (state.is_xmm_copy(dst_index) && state.is_xmm_copy(src_index) &&
+                                             state.get_xmm_copy(dst_index) == state.get_xmm_copy(src_index));
+                    if (same_value && src_index != dst_index) {
+                        // Patterns like this is seen in some games:
+                        //   movaps xmm1,xmm2
+                        //   movaps xmm0,xmm2
+                        //   shufps xmm0,xmm2,0x0
+                        //   shufps xmm1,xmm2,0x55
+                        //   shufps xmm2,xmm2,0xaa
+                        // This pattern of move + shufps of moved register can become a single vgather.vi
+                        // Unfortunately, the compilers would choose to emit `shufps xmm0, xmm2, 0` instead of `shufps xmm0, xmm0, 0` which
+                        // would allow us to emit a vgather.vi without further analysis. Games would also not choose to emit the pshufd variant
+                        // which would be one instruction instead of two because of domain crossing. For these reasons we detect it ourselves
+                        // and replace it with a pshufd as we don't care about domains. Since pshufd only uses the src elements it is better
+                        // for RVV translations, as the worst case will use 1 vrgather instead of 2.
+                        // Replace mnemonic and operand, keep instruction length the same for translation_sizes reasons
+                        // Also don't omit the copy, keeps synchronous signal state correct if a fault happens after the copy
+                        instruction.mnemonic = ZYDIS_MNEMONIC_PSHUFD;
+                    }
+                    state.remove_xmm_copy(dst_index);
+                } else {
+                    state.remove_xmm_copy(dst_index);
+                }
+            } else if (is_ymm) {
+                const int index = operands[i].reg.value - ZYDIS_REGISTER_YMM0;
+                state.remove_xmm_copy(index);
+            }
+        }
+    }
+}
+
 void Optimizer::native_pass(u8* start, u64 size) {
     static const u32 acq_fence = []() {
         u32 instr;
